6cd1b277bc3da84034ddfe7c78d5756771f41a87
Vlad-Skubskiy <127733341+Vlad-Skubskiy@users.noreply.github.com>
2024-09-17 12:54:04 +0300

    Add files via upload

---
+++ movie.py
@@ -0,0 +1,23 @@
+# first
+movie = "Bad boys"
+print(movie)
+
+# second
+is_employee = True
+is_employer = False
+print(is_employee and is_employer)
+
+#third
+is_sunny = False
+is_raining = True
+
+print(not is_sunny)
+print(not is_raining)
+
+#math
+import math
+
+x=2.531
+y=0.193
+result = y * ((x * y) ** (1 / 3)) + y * math.sin(x * y) - ((math.e) ** ((-x) * y)) * math.cos(x * y) + 7.1/ (x * y)
+print(result)
